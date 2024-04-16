c7986a0d1bca0aa35fa143d1f66a691b23071a9f
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2024-04-16 20:59:05 +0100

    Completed basic functionality

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
- const emailCheck=(data)=>{
-const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-let checkRegex = emailRegex.test(data);
-return checkRegex ? "Email is valid": "Email is invalid";
-}
-module.exports = emailCheck;
+const emailCheck=(data)=>{
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    let checkRegex = emailRegex.test(data);
+    return checkRegex ? "Email is valid": "Email is invalid";
+    }
+    
+export default emailCheck;
